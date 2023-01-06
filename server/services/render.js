108eeb66ea9df71498f61cc953061516428c8625
Akhtarbilal97 <80459974+Akhtarbilal97@users.noreply.github.com>
2023-01-05 20:21:00 -0800

    show data in a table

--- server/services/render.js
+++ server/services/render.js
@@ -1,8 +1,21 @@
+const axios = require('axios');
+
+
 exports.homeRoutes = (req, resp) => {
-  resp.render("index");
+  // Make a get request to/api/users
+  axios.get('http://localhost:8080/api/users')
+  .then(function(response){
+    resp.render('index',{users: response.data});
+
+  })
+  .catch(err =>{
+    resp.send(err)
+  })
+  
 };
 
 exports.add_user = (req, resp) => {
+  axios.post('http://localhost:8080/api/users')
   resp.render("add_user");
 };
  
